ab0f28ac4aaa2990e7f3b00d95b2b3f39432d455
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-02-12 23:22:00 +0400

    add environment variables

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index');
 
 var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
-// mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-// mongoose.connect('mongodb+srv://Hudson:nowordforpassword@yelpcamp-yy3de.mongodb.net/test?retryWrites=true&w=majoritymongodb+srv://Hudson:nowordforpassword@cluster0-yy3de.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.use(bodyParser.urlencoded({
