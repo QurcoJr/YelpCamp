01147e809a417175479ea04c2b95a1061bdaf6e3
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-02-13 00:37:34 +0400

    remove environment variables

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index');
 
 // var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.connect('mongodb+srv://Hudson:nowordforpassword@yelpcamp-yy3de.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.use(bodyParser.urlencoded({
