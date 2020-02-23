1d176d0d7725e3a650131a0ac9e9e5fd07eeaeac
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-02-23 16:14:02 +0400

    UI imrpove

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var campgroundRoutes = require('./routes/campgrounds'),
     commentRoutes = require('./routes/comments'),
     indexRoutes = require('./routes/index');
 
-// var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
+var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.connect('mongodb+srv://Hudson:nowordforpassword@cluster0-yy3de.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.use(bodyParser.urlencoded({
